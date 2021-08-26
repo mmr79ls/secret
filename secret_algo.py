4d0f1514b714083329c1742dd265fb5d30dac06e
mmr79ls <88501423+mmr79ls@users.noreply.github.com>
2021-08-26 21:19:05 +0200

    Update secret_algo.py

--- secret_algo.py
+++ secret_algo.py
@@ -26,6 +26,7 @@ footer {visibility: hidden;}
 
 """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+st.write("DB username:", st.secrets["api_key"])
 api_key=st.secrets["api_key"]
 api_secret=st.secret["api_secret"]
 client = Client(api_key, api_secret)
