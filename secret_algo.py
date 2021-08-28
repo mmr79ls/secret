94c1a6d260ea4c17c7b3e42cd97d1bd13b294945
mmr79ls <88501423+mmr79ls@users.noreply.github.com>
2021-08-28 16:04:41 +0200

    Update secret_algo.py

--- secret_algo.py
+++ secret_algo.py
@@ -116,6 +116,9 @@ def pump(symbol,profit_flag=1,tf='15m',duration=6):
                     p=pd.DataFrame()
                     p=z[z.index>i]
                     z['profit'][i]=p.Close.max()*100/z[z.index==i].Close.max()
+                    st.write(t.Close.idxmax())
+                    st.write(i)
+                    st.write(t.Close.idxmax()-i)
                     z['profit_duration'][i]=str(t.Close.idxmax()-i)
                     z['loss']=p.Close.min()*100/z[z.index==i].Close.max()
                     z['loss_duration'][i]=str(t.Close.idxmin()-i)
