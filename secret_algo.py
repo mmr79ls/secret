8b950b540b18d677b1beb15e16a6442e3e097c40
mmr79ls <88501423+mmr79ls@users.noreply.github.com>
2021-08-29 21:09:51 +0200

    Update secret_algo.py

--- secret_algo.py
+++ secret_algo.py
@@ -331,7 +331,7 @@ symbols_f=df[df['signal']!=0].symbol.unique()
 st.write(len(symbols_f))
 symbol=st.sidebar.radio('Symbol',symbols_f)
           
-fig,z=plot_symbol(symbol,profit=0,tf,duration)
+fig,z=plot_symbol(symbol,profit=0,tf='15m',duration='2 day')
 st.write(fig)
 st.dataframe(df)
 st.dataframe(df1)
