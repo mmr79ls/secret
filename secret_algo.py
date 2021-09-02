d562a177180f639d8517baba27bbf686f03eb556
mmr79ls <88501423+mmr79ls@users.noreply.github.com>
2021-09-02 08:52:20 +0200

    Update secret_algo.py

--- secret_algo.py
+++ secret_algo.py
@@ -60,7 +60,7 @@ def get_orderbook(symbol):
     f1=[]
     for i in range(1,len(bins)):
         f1.append(data[(data.index>bins[i-1]) & (data.index<bins[i])].quantity.sum())
-    st.write(f1)
+    st.write(bins)
     f=pd.DataFrame([bins,f1]).T
     f.columns=columns=['price','quantity']
     f['side']='bids'
