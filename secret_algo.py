1e8359e1dee667161d8581b606c8cea79435d91d
mmr79ls <88501423+mmr79ls@users.noreply.github.com>
2021-10-06 00:18:22 +0200

    Update secret_algo.py

--- secret_algo.py
+++ secret_algo.py
@@ -330,15 +330,9 @@ def plot_symbol(symbol,profit=0,tf='15m',duration='2 day'):
     fig.update_layout(
     dragmode='drawopenpath',
     newshape_line_color='cyan',
-    title_text='Draw a path to separate versicolor and virginica',
-    modebar_add=['drawline',
-        'drawopenpath',
-        'drawclosedpath',
-        'drawcircle',
-        'drawrect',
-        'eraseshape'
-       ]
+    title_text='Draw a path to separate versicolor and virginica'
     )
+
     #fig.add_vline(x=z['tmp'].max(), line_width=3, line_dash="dash", line_color="green")
     fig.update_layout(showlegend=False)
     fig.update_layout( height=400,width=800, margin=dict(r=5, l=5, t=5, b=5))
@@ -406,7 +400,14 @@ st.write('wint rate ',str(round(profit*100/len(df1),2)))
 symbol=st.sidebar.radio('Symbol',symbols_f)
 
 fig,z=plot_symbol(symbol,0,tf=tf,duration=6)
-st.write(fig)
+config={'modeBarButtonsToAdd':['drawline',
+                                        'drawopenpath',
+                                        'drawclosedpath',
+                                        'drawcircle',
+                                        'drawrect',
+                                        'eraseshape'
+                                       ]}
+st.write(fig,config=config)
 df=df.drop(columns=['Open','High','Low'],axis=1)
 
 st.dataframe(df1)
